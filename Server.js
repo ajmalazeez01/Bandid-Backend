a30ba77f72355e141ef25c34245f88aa618441aa
ajmalazeez01 <ajmalazeez776@gmail.com>
2023-11-08 20:55:54 +0530

    ff

--- Server.js
+++ Server.js
@@ -20,7 +20,7 @@ mongoose();
 app.use(morgan("dev"))
 
 app.use((req, res, next) => {                      
-  res.setHeader('Access-Control-Allow-Origin', "http://localhost:5000");
+  res.setHeader('Access-Control-Allow-Origin', "https://bandidonline.netlify.app/");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true'); // Enable credentials
